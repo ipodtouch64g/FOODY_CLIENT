b727b275c43f95b65dc984020102f569216266f0
QAZXWS <qazwyt3196@gmail.com>
2017-05-12 14:30:50 +0800

    bg ok

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,60 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom'
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Input,
-    Button
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Input,
+  Button
 } from 'reactstrap';
-
-
-
-
-
-
 import './Main.css';
 
 export default class Main extends React.Component {
 
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
+  render() {
+    return (
+      <Router>
+        <div className='main'>
+          <div className='bg'>
+          <Container fluid>
+              <div className='container d-flex flex-column  justify-content-between align-items:center main-title'>
+                <div></div>
+                <div className="mx-auto wow fadeInDown">
+                  <div>
+                    <h1>FOODY |</h1>
+                  </div>
+                  <div>
+                    <h3>your choice for food</h3>
+                  </div>
+                </div>
+                <div className='container align-items:center searchBar'>
 
+                </div>
 
-
-
-    }
-
-    render() {
-        return (
-            <Router>
-                <div className='main'>
-                    <div className='bg-faded'>
-                        <div className='container'>
-
-                        </div>
-                    </div>
-
-                    {/* <Route exact path="/" render={() => (
+              </div>
+          </Container>
+          </div>
+          {/* <Route exact path="/" render={() => (
 
                     )}/> */}
 
-                    <div className='footer'>
-                        DataLab.
-                    </div>
-                </div>
-            </Router>
-        );
-    }
+          <div className='footer'>
+            FOODY.
+          </div>
+        </div>
+      </Router>
+    );
+  }
 
 }
